4984648cb0b63ea1aa80f71af60440836876537f
Petr Milyutin <petrmil8@gmail.com>
2017-10-28 20:49:31 +0600

    доровнял оставшиеся элементы; псевдоэлемент, добовлявший три линии с тенью заменил на div, но проблема с hover'ом для all-active-completed осталась

--- src/todolist.js
+++ src/todolist.js
@@ -5,32 +5,33 @@ import './style.css';
 export class ToDoList extends React.Component {
   render() {
   	return(
-		<div className='wrap'>
+		<div>
+			<h1>todos</h1>
 			<div className='container'>
-				<h1>todos</h1>
-				  	<div className='myList'>
-				  		<input type='checkbox' className='toggleAll' />
-				  		<input className='newItem'placeholder='What needs to be done?' />
-				  		<ul className='todo-list'>
-				  			<li>
-				  				<input type='checkbox' className='toggle' /> 
-				  				<label htmlFor='toggle'></label> <span>Первый пункт</span>
-				  			</li>
-				  		</ul>
-				  		<footer className='upFooter'>
-				  			<span className='count'>1 item left</span>
-				  			<ul className='filters'>
-				  				<li>
-				  				<a href="">All</a>
-				  				</li>
-				  				<li>
-				  				<a href="">Active</a>
-				  				</li>
-				  				<li>
-				  				<a href="">Completed</a>
-				  				</li>
-				  			</ul>
-				  		</footer>
+			  	<div className='myList'>
+				  	<input type='checkbox' className='toggleAll' />
+				  	<input className='newItem'placeholder='What needs to be done?' />
+			  		<ul className='todo-list'>
+			  			<li>
+			  				<input type='checkbox' className='toggle' /> 
+			  				<label htmlFor='toggle'></label> <span>Первый пункт</span>
+			  			</li>
+			  		</ul>
+			  		<footer className='upFooter'>
+			  			<span className='count'>1 item left</span>
+			  			<ul className='filters'>
+			  				<li>
+			  				<a href="">All</a>
+			  				</li>
+			  				<li>
+			  				<a href="">Active</a>
+			  				</li>
+			  				<li>
+			  				<a href="">Completed</a>
+			  				</li>
+			  			</ul>
+			  		</footer>
+			  		<div className='threeLine'></div>
 				  	</div>
 			</div>
 				<footer className='downFooter'>
